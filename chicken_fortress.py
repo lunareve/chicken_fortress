af9e1d755420bea15d3a8fecf9c671b35c366dbf
Connie Chow <clionesse@yahoo.com>
2018-03-30 11:14:39 -0700

    add .gitignore, fix globals

--- chicken_fortress.py
+++ chicken_fortress.py
@@ -1,5 +1,6 @@
 """Turns switches on and off for raspberry pi according to time of day."""
 
+import RPi.GPIO as GPIO
 import datetime
 import time
 import pytz
@@ -9,16 +10,31 @@ from astral import Astral
 a = Astral()
 a.solar_depression = 'astronomical'
 city = a['San Francisco']
-door_status = 'open'
+door_status = ['open']
+door_motor_forwards = 2
+door_motor_backwards = 3
 
+# Setup Rasberrry Pi
+GPIO.setmode(GPIO.BCM)
+
+# Functions
 def open_door():
-    #Turn on the Switch
-    GPIO.output(2, GPIO.LOW)
+    #Turn on the switch
+    GPIO.setup(door_motor_forwards, GPIO.OUT)
+    GPIO.output(door_motor_forwards, GPIO.HIGH)
+    time.sleep(6)
     #Turn off the switch
+    GPIO.output(door_motor_forwards, GPIO.LOW)
     GPIO.cleanup()
 
 def close_door():
-    pass
+    #Turn on the switch
+    GPIO.setup(door_motor_backwards, GPIO.OUT)
+    GPIO.output(door_motor_backwards, GPIO.HIGH)
+    time.sleep(6)
+    #Turn off the switch
+    GPIO.output(door_motor_backwards, GPIO.LOW)
+    GPIO.cleanup()
 
 def is_day(t, sun):
     return t > sun['sunrise'] and t < sun['sunset']
@@ -28,11 +44,12 @@ def main():
     t = datetime.datetime.now(pytz.utc)
     today = datetime.date.today()
     sun = city.sun(date=today, local=True)
-
+    global door_status
     # check if door is open or closed
     # if daylight and door is closed, open door
     # if daylight and door is open, do nothing
     if door_status == 'closed' and is_day(t, sun):
+        time.sleep(3600)
         open_door()
         door_status = 'open'
 
@@ -42,6 +59,7 @@ def main():
         close_door()
         door_status = 'closed'
 
+# Keep running every 15 min
 while True:
     main()
     time.sleep(900)
